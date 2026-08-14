f731c5eff5bf97caba6a136bf610bc1c0626f34a
Jimi <jimi.ademola1@gmail.com>
2026-08-14 16:24:32 -0600

    build

--- apps/photos/astro.config.mjs
+++ apps/photos/astro.config.mjs
@@ -10,4 +10,9 @@ export default defineConfig({
   site: "https://www.photos.jimiademola.com",
   integrations: [react()],
   adapter: cloudflare(),
+  vite: {
+    resolve: {
+      tsconfigPaths: false,
+    },
+  },
 });
